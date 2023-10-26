3860ab2cef438cb9b9174a331b51aaac49ae4bc6
Warsame Egal <warsameegal0@gmail.com>
2023-10-25 20:35:34 -0400

    update port

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,6 @@ app.get("/api", (req, res) => {
   populate();
 });
 
-app.listen(8080, () => {
-  console.log("Server started on port 8080");
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
